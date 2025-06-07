dab09fee48ac8d5eccef2a934e61af15f0d7bff7
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2025-06-06 23:29:29 -0300

    Ajustes spec cadastro de herói

--- client/cypress/e2e/spec-cadastro-heroi.cy.js
+++ client/cypress/e2e/spec-cadastro-heroi.cy.js
@@ -9,19 +9,6 @@ const loginPage = new LoginPage();
 const cadastroHeroiPage = new CadastroHeroiPage();
 
 describe('Testes de cadastro de herói', () => {
-/*     const seletoresCadHeroi = {
-        btnCadastroHeroi: 'button',
-        campoName: "[name='name']",
-        campoPrice: "[name='price']",
-        campoFans: "[name='fans']",
-        campoSaves: "[name='saves']",
-        campoPowers: "[data-cy='powersSelect']",
-        campoAvatarHeroi: "[type='file']",
-        btnSubmitCadHeroi: 'button',
-        nomeHeroiCadastrado: "[data-cy='name']",
-        verificadorPreenchimentoCampos: '.text-red-500'
-    } */
-    
     beforeEach(() => {
         cy.clearLocalStorage(); // Limpa o armazenamento local antes de cada teste
         loginPage.acessaPaginaInicial(); // acessa a página inicial
@@ -53,14 +40,5 @@ describe('Testes de cadastro de herói', () => {
         cadastroHeroiPage.cadastraHeroi();
         cadastroHeroiPage.verificaPreenchimentoCampos('Price is required', 0);
         cadastroHeroiPage.verificaPreenchimentoCampos('Powers is required', 1);
-
-        /* cy.get(seletoresCadHeroi.btnCadastroHeroi).contains('Create New Hero').click();
-        cy.get(seletoresCadHeroi.campoName).type(dadosHeroi.heroiFalha.name);
-        cy.get(seletoresCadHeroi.campoFans).type(dadosHeroi.heroiFalha.fans);
-        cy.get(seletoresCadHeroi.campoSaves).type(dadosHeroi.heroiFalha.saves);
-        cy.get(seletoresCadHeroi.campoAvatarHeroi).selectFile('cypress/fixtures/super-sopradora.png'); // seleciona o arquivo de avatar do herói
-        cy.get(seletoresCadHeroi.btnSubmitCadHeroi).contains('Submit').click();
-        cy.get(seletoresCadHeroi.verificadorPreenchimentoCampos).should('contain', 'Price is required'); // verifica se a mensagem de erro do campo Price é exibida
-        cy.get(seletoresCadHeroi.verificadorPreenchimentoCampos).should('contain', 'Powers is required'); // verifica se a mensagem de erro do campo Powers é exibida */
     });
 });
